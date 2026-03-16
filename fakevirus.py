21a88a06525a8095272f1c05f2f320dbeb7bdba8
babitaself0-hub <babitaself0@gmail.com>
2026-03-16 08:59:15 +0530

    Create fakevirus.py

---
+++ fakevirus.py
@@ -0,0 +1,14 @@
+print("=================================")
+print("WARNING: VIRUS DETECTED")
+print("=================================")
+
+print("Scanning system files...")
+print("Checking memory...")
+print("Checking network...")
+print("Checking storage...")
+
+print("Threat found: Trojan.fake.demo")
+print("Removing threat...")
+
+print("System secured.")
+print("No real virus found. This was a demo.")
